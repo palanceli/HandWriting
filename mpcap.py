18c402f7f38e5efc6aa8a66ef4796dc86384a7c0
palance <palanceli@163.com>
2017-10-10 21:46:16 +0800

    commit changes

--- mpcap.py
+++ mpcap.py
@@ -11,6 +11,14 @@ import scipy
 import scipy.interpolate
 import unittest
 
+def WaitToClose(img):
+	while True:
+		cv2.imshow('image', img)
+		if cv2.waitKey(20) & 0xFF == 27:
+			break
+	
+	cv2.destroyAllWindows()
+
 class Cap(object):
 	FACTOR = 1
 	def __init__(self, x, y):
@@ -37,29 +45,98 @@ class Cap(object):
 		img[y : y+h, x : x+w] = dst
 		return img
 
+class CapTanPointHelper(object):
+	def __init__(self):
+		self.capX, self.capY = 25, 25
+		self.cap = Cap(self.capX, self.capY)
+
+		self.bgWidth, self.bgHeight = 50, 50
+		self.bgImg = numpy.zeros((self.bgWidth, self.bgHeight, 3), numpy.uint8)
+		self.bgImg[:, :] = (0, 0, 0)
+
+		self.lineColor = 31
+
+		self.radius = 40
+
+	def getArmLine(self, angle, step):
+		line = {'startPt': (
+			int(self.cap.x - self.radius * math.cos(math.radians(angle)) + step * math.sin(math.radians(angle))),
+			int(self.cap.y - self.radius * math.sin(math.radians(angle)) - step * math.cos(math.radians(angle)))) ,
+			'endPt': (
+			int(self.cap.x + self.radius * math.cos(math.radians(angle)) + step * math.sin(math.radians(angle))),
+			int(self.cap.y + self.radius * math.sin(math.radians(angle)) - step * math.cos(math.radians(angle))))}
+		return line
+
+	def addArmLineToCap(self, armLine):
+		capImg = self.bgImg.copy()
+		self.cap.Paste2Img(capImg)
+		bgImg = self.bgImg.copy()
+		cv2.line(bgImg, armLine['startPt'], armLine['endPt'], (self.lineColor, self.lineColor, self.lineColor), 1)
+		# logging.debug('capImg:%s, img:%s' % (capImg.shape, img.shape))
+		dst = cv2.add(capImg, bgImg)
+		return dst
+
+	def getCoveredPoints(self, img):
+		coveredPoints = []
+		rows, cols, channels = img.shape
+		RGB2Str = lambda clr : '%03d%03d%03d' % (clr[0], clr[1], clr[2])
+		for r in range(rows):
+			for c in range(cols):
+				if RGB2Str(img[r, c]) == '000000000' or RGB2Str(img[r, c]) == '031031031':
+					continue
+
+				coveredPoints.append([c, r])
+		return coveredPoints
+
+	def getTanPointOnSingleSide(self, angle, left=True):
+		# 给定角度，求单侧切点
+		prevImg = None
+		prevCoveredPoints = None
+		for step in range(1, 50):
+			if not left:
+				step *= -1
+			armLine = self.getArmLine(angle, step)
+			img = self.addArmLineToCap(armLine)
+			coveredPoints = self.getCoveredPoints(img)
+			if len(coveredPoints) > 0:
+				prevImg = img
+				prevCoveredPoints = coveredPoints
+			else:
+				idx = len(prevCoveredPoints) / 2
+				tanPt = prevCoveredPoints[idx]
+				# logging.debug(tanPt)
+				# cv2.polylines(prevImg, numpy.array([tanPt], numpy.int32).reshape(-1, 1, 2), True, (0, 0, 255), 1)
+				# WaitToClose(prevImg)
+				return tanPt
+		return None
+
+	def GetTanPoints(self, angle):	
+		# 给定角度，求左右两个切点，角度是正方向→顺时针，返回的是相对cap中心的偏移
+		lpt = self.getTanPointOnSingleSide(angle, left=True)
+		rpt = self.getTanPointOnSingleSide(angle, left=False)
+		lpt[0] -= self.capX
+		lpt[1] -= self.capY
+		rpt[0] -= self.capX
+		rpt[1] -= self.capY
+		return lpt, rpt
+
 class CapUT(unittest.TestCase):
 	def setUp(self):
 	    logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
 	    logging.basicConfig(level=logging.DEBUG, format=logFmt, datefmt='%H:%M',)
 
-	def waitToClose(self, img):
-		while True:
-			cv2.imshow('image', img)
-			if cv2.waitKey(20) & 0xFF == 27:
-				break
-		
-		cv2.destroyAllWindows()
-
 	def test01(self):
-		img = numpy.zeros((300, 300, 3), numpy.uint8)
-		img[:, :] = (255, 255, 255)
+		capTPHelper = CapTanPointHelper()
+		lpt, rpt = capTPHelper.GetTanPoints(202)
 
-		cap = Cap(100, 100)
-		cap.Paste2Img(img)
-		self.waitToClose(img)
+	def test02(self):
+		capTPHelper = CapTanPointHelper()
+		for angle in range(0, 360):
+			lpt, rpt = capTPHelper.GetTanPoints(angle)
+			logging.debug('angle:%3d, l:(%3d, %3d), r:(%3d, %3d)' % (angle, lpt[0], lpt[1], rpt[0], rpt[1]))
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
     logging.basicConfig(level=logging.DEBUG, format=logFmt, datefmt='%H:%M',)
     unittest.main
-    # cmd: python -m unittest mpcap.CapUI.Test01
+    # cmd: python -m unittest mpcap.CapUT.test01
